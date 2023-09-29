6949b1e1abbb1ff090248d66e0e59ac303235da1
Shubham Patil <stp00007@gmail.com>
2023-09-29 20:35:03 +0530

    sec

--- src/test/java/com/flipkart/search/GoogleSearch.java
+++ src/test/java/com/flipkart/search/GoogleSearch.java
@@ -1,12 +1,9 @@
 package com.flipkart.search;
 
 import java.time.Duration;
-
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
@@ -31,5 +28,6 @@ public class GoogleSearch {
 		
 		driver.findElement(By.name("q")).sendKeys("selenium");
 		driver.quit();
+		
 	}
 }
